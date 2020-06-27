50127e37efd3d842883d573719fc62e78a4a55bc
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-06-27 19:09:33 -0400

    crown project route modification

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route } from "react-router-dom"
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+
 
 function App() {
+
+  const HatsPage = () => (
+    <div>
+      <h1>Htas Page</h1>
+    </div>
+  )
+
   return (
     <div>
-      <Homepage/>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { withRouter } from "react-router-dom"
+import "./menu-item.styles.scss"
+
+function MenuItem({ title, imageUrl, size, history, linkUrl, match }) {
+   
+    return (
+        <div className={`menu-item ${size ? size: "" }}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <div  style={{ backgroundImage: `url(${imageUrl})` }}
+                className="background-image" />
+                <div className="content">
+                    <h1 className="title">{ title.toUpperCase() }</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+        </div>
+    )
+}
+
+export default withRouter(MenuItem)
